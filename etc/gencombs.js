69bafbf0fc60b9f3f72abd56136077d7893bc56d
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-11-02 07:54:07 -0700

    combs: add precomputed combs.

--- etc/gencombs.js
+++ etc/gencombs.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs');
 const Path = require('path');
+const SHA256 = require('bcrypto/lib/sha256');
 const Goo = require('../lib/js/goo');
 
 const moduli = [
@@ -17,6 +18,9 @@ const sizes = [
   // 2048,
   4096
 ];
+
+const MAX_SIZE = 510;
+const MAX_LEN = 2;
 
 function mkdir(path, mode = 0o755) {
   path = Path.resolve(__dirname, '..', ...path);
@@ -36,39 +40,78 @@ function write(path, txt) {
 }
 
 function combToC(lines, name, comb) {
-  lines.push(`    .${name} = {`);
-  lines.push(`      .points_per_add = ${comb.pointsPerAdd},`);
-  lines.push(`      .adds_per_shift = ${comb.addsPerShift},`);
-  lines.push(`      .shifts = ${comb.shifts},`);
-  lines.push(`      .bits_per_window = ${comb.bitsPerWindow},`);
-  lines.push(`      .bits = ${comb.bits},`);
-  lines.push(`      .points_per_subcomb = ${comb.pointsPerSubcomb},`);
-  lines.push(`      .size = ${comb.size},`);
-  lines.push(`      .items = {`);
+  lines.push(`      .${name} = {`);
+  lines.push(`        .points_per_add = ${comb.pointsPerAdd},`);
+  lines.push(`        .adds_per_shift = ${comb.addsPerShift},`);
+  lines.push(`        .shifts = ${comb.shifts},`);
+  lines.push(`        .bits_per_window = ${comb.bitsPerWindow},`);
+  lines.push(`        .bits = ${comb.bits},`);
+  lines.push(`        .points_per_subcomb = ${comb.pointsPerSubcomb},`);
+  lines.push(`        .size = ${comb.size},`);
+  lines.push(`        .items = {`);
 
   for (const item of comb.items)
-    lines.push(`        "${item}",`);
+    lines.push(`          "${item}",`);
 
+  lines.push(`        },`);
   lines.push(`      },`);
-  lines.push(`    },`);
 }
 
 function combItemToC(lines, item) {
-  lines.push(`  {`);
+  lines.push(`    {`);
   combToC(lines, 'g', item.g);
   combToC(lines, 'h', item.h);
-  lines.push(`  },`);
+  lines.push(`    },`);
 }
 
-function combsToC(name, bits, json) {
-  const def = name.toUpperCase();
+const HEADER = `
+typedef struct goo_precomb_s {
+  long points_per_add;
+  long adds_per_shift;
+  long shifts;
+  long bits_per_window;
+  long bits;
+  long points_per_subcomb;
+  long size;
+  const char *items[${MAX_SIZE}];
+} goo_precomb_t;
+
+typedef struct goo_precomb_item_s {
+  goo_precomb_t g;
+  goo_precomb_t h;
+} goo_precomb_item_t;
+
+typedef struct goo_precombs_s {
+  const char hash[64];
+  long size;
+  goo_precomb_item_t items[${MAX_LEN}];
+} goo_precombs_t;
+`;
+
+function combsToC(name, bits, modulus, json) {
+  const hash = SHA256.digest(modulus).toString('hex');
+  const suffix = bits == null ? '' : `_${bits}`;
+  const def = name.toUpperCase() + suffix;
+  const len = json.length;
   const lines = [];
 
+  lines.push(HEADER);
   lines.push(`static const goo_precombs_t GOO_COMB_${def} = {`);
+  lines.push(`  .hash = "${hash}",`);
+  lines.push(`  .size = ${len},`);
+  lines.push(`  .items = {`);
 
   for (const item of json)
     combItemToC(lines, item);
 
+  let i = MAX_LEN - json.length;
+
+  while (i > 0) {
+    lines.push('  NULL,');
+    i -= 1;
+  }
+
+  lines.push(`  },`);
   lines.push(`};`);
 
   return lines.join('\n');
@@ -84,7 +127,7 @@ function main() {
       const suffix = bits == null ? '' : `-${bits.toString(10)}`;
       const file = `${name}${suffix}`;
       const str = JSON.stringify(json, null, 2);
-      const c = combsToC(name, bits, json);
+      const c = combsToC(name, bits, modulus, json);
 
       write(['combs', `${file}.json`], str + '\n');
       write(['combs', `${file}.h`], c + '\n');
