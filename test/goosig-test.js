366aa4c70dadc9c14bf2b453078d974872d3e3cb
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-11-02 03:45:05 -0700

    goo: expose native flag.

---
+++ test/goosig-test.js
@@ -0,0 +1,22 @@
+/* eslint-env mocha */
+/* eslint prefer-arrow-callback: 'off' */
+
+'use strict';
+
+const assert = require('./util/assert');
+const Goo = require('../');
+
+describe('GooSig', function() {
+  it('should have correct environment', () => {
+    switch (process.env.NODE_BACKEND) {
+      case 'js':
+      case 'node':
+        assert.strictEqual(Goo.native, 0);
+        break;
+      case 'native':
+      default:
+        assert.strictEqual(Goo.native, 2);
+        break;
+    }
+  });
+});
