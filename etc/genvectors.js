ed6fd6ece8c77138247947177518eca97387b21a
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-11-02 10:20:06 -0700

    test: more test vectors.

---
+++ etc/genvectors.js
@@ -0,0 +1,80 @@
+/* eslint camelcase: "off" */
+
+'use strict';
+
+const assert = require('bsert');
+const Path = require('path');
+const fs = require('fs');
+const rsa = require('bcrypto/lib/rsa');
+const random = require('bcrypto/lib/random');
+const SHA256 = require('bcrypto/lib/sha256');
+const JS = require('../lib/js/goo');
+const Native = require('../lib/native/goo');
+const Signature = require('../lib/js/signature');
+
+const vectors = [];
+
+for (const Goo of [JS, Native]) {
+  const moduli = [
+    ['AOL1', Goo.AOL1],
+    ['AOL2', Goo.AOL2],
+    ['RSA2048', Goo.RSA2048],
+    ['RSA617', Goo.RSA617]
+  ];
+
+  const sizes = [
+    1024,
+    2048,
+    4096
+  ];
+
+  const gh = [
+    [2, 3],
+    [5, 7]
+  ];
+
+  for (const [name, modulus] of moduli) {
+    for (const [g, h] of gh) {
+      const ver = new Goo(modulus, g, h, null);
+
+      for (const bits of sizes) {
+        const goo = new Goo(modulus, g, h, bits);
+
+        console.log('Generating 5 vectors for %s.', name);
+        console.log('  g=%d, h=%d, bits=%d', g, h, bits);
+
+        for (let i = 0; i < 5; i++) {
+          const msg = SHA256.digest(random.randomBytes(32));
+          const key = rsa.privateKeyGenerate(bits);
+          const [s_prime, C1] = goo.challenge(key);
+          const ct = goo.encrypt(s_prime, C1, key);
+          const sig = goo.sign(msg, s_prime, C1, key);
+          const result = ver.verify(msg, sig, C1);
+
+          assert.strictEqual(result, true);
+
+          vectors.push({
+            group: name,
+            groupBits: goo.bits,
+            g: g,
+            h: h,
+            bits: bits,
+            key: rsa.privateKeyExportJWK(key),
+            msg: msg.toString('hex'),
+            s_prime: s_prime.toString('hex'),
+            C1: C1.toString('hex'),
+            ct: ct.toString('hex'),
+            sig: Signature.decode(sig, goo.bits).toJSON(),
+            original: false,
+            native: Goo.native ? true : false
+          });
+        }
+      }
+    }
+  }
+}
+
+{
+  const path = Path.resolve(__dirname, '..', 'test', 'data', 'vectors2.json');
+  fs.writeFileSync(path, JSON.stringify(vectors, null, 2) + '\n');
+}
--- test/goo-test.js
+++ test/goo-test.js
@@ -5,39 +5,33 @@
 'use strict';
 
 const assert = require('./util/assert');
-// const rsa = require('bcrypto/lib/rsa');
+const rsa = require('bcrypto/lib/rsa');
 const SHA256 = require('bcrypto/lib/sha256');
 const testUtil = require('./util');
-const Goo = require('../lib/js/goo');
+const JS = require('../lib/js/goo');
 const Native = require('../lib/native/goo');
 const Signature = require('../lib/js/signature');
-const vectors = require('./data/vectors-original.json');
-const vectorsNative = require('./data/vectors-native.json');
+const vectors = require('./data/vectors.json');
 
-function runTests(name, Goo, Other, vectors) {
+function runTests(name, Goo, Other) {
   describe(name, function() {
     this.timeout(60000);
 
     // 4096-bit GoUO
     // 4096-bit RSA GoUO, 2048-bit Signer key
-    const gops_4_2_p = new Goo(Goo.AOL2, 2, 3, 2048);
+    const gops_4_2_p = () => new Goo(Goo.AOL2, 2, 3, 2048);
     // 4096-bit RSA GoUO, 4096-bit Signer key
-    const gops_4_4_p = new Goo(Goo.AOL2, 2, 3, 4096);
+    const gops_4_4_p = () => new Goo(Goo.AOL2, 2, 3, 4096);
     // 4096-bit RSA GoUO (verification)
-    const gops_4_v = new Goo(Goo.AOL2, 2, 3, null);
+    const gops_4_v = () => new Goo(Goo.AOL2, 2, 3, null);
 
     // 2048-bit GoUO
     // 2048-bit RSA GoUO, 2048-bit Signer key
-    const gops_2_2_p = new Goo(Goo.RSA2048, 2, 3, 2048);
+    const gops_2_2_p = () => new Goo(Goo.RSA2048, 2, 3, 2048);
     // 2048-bit RSA GoUO, 4096-bit Signer key
-    const gops_2_4_p = new Goo(Goo.RSA2048, 2, 3, 4096);
+    const gops_2_4_p = () => new Goo(Goo.RSA2048, 2, 3, 4096);
     // 2048-bit RSA GoUO (verification)
-    const gops_2_v = new Goo(Goo.RSA2048, 2, 3, null);
-
-    // Native 4096 bit
-    const other_4_v = new Other(Goo.AOL2, 2, 3, null);
-    // Native 2048 bit
-    const other_2_v = new Other(Goo.RSA2048, 2, 3, null);
+    const gops_2_v = () => new Goo(Goo.RSA2048, 2, 3, null);
 
     const tests = [
       ['4096-bit RSA GoUO, 2048-bit Signer PK', gops_4_2_p, gops_4_v],
@@ -50,8 +44,11 @@ function runTests(name, Goo, Other, vectors) {
 
     const created = [];
 
-    for (const [i, [name, gops_p, gops_v]] of tests.entries()) {
+    for (const [i, [name, gops_p_, gops_v_]] of tests.entries()) {
       it(`should sign and verify msg: "${name}"`, () => {
+        const gops_p = gops_p_();
+        const gops_v = gops_v_();
+
         const msg = SHA256.digest(Buffer.from(name, 'binary'));
 
         // Random signer modulus.
@@ -80,6 +77,11 @@ function runTests(name, Goo, Other, vectors) {
     }
 
     it('should verify with opposite implementation', () => {
+      // Native 4096 bit
+      const other_4_v = new Other(Goo.AOL2, 2, 3, null);
+      // Native 2048 bit
+      const other_2_v = new Other(Goo.RSA2048, 2, 3, null);
+
       for (let i = 0; i < created.length; i++) {
         const group = i < 2 ? other_4_v : other_2_v;
         const [msg, sig, C1] = created[i];
@@ -89,24 +91,57 @@ function runTests(name, Goo, Other, vectors) {
     });
 
     for (const vector of vectors) {
+      const name = vector.group;
       const group = Goo[vector.group];
+      const groupBits = vector.groupBits;
+      const g = vector.g;
+      const h = vector.h;
       const bits = vector.bits;
-      // const key = rsa.privateKeyImportJWK(vector.key);
+      const key = rsa.privateKeyImportJWK(vector.key);
       const msg = Buffer.from(vector.msg, 'hex');
-      // const s_prime = Buffer.from(vector.s_prime, 'hex');
+      const s_prime = Buffer.from(vector.s_prime, 'hex');
       const C1 = Buffer.from(vector.C1, 'hex');
-      const gbits = vector.group === 'AOL2' ? 4096 : 2048;
-      const sig = Signature.fromJSON(vector.sig).encode(gbits);
-      const str = `${gbits}-bit RSA GoUO, ${bits}-bit Signer PK`;
+      const ct = Buffer.from(vector.ct, 'hex');
+      const sig = Signature.fromJSON(vector.sig).encode(groupBits);
+      const str = `${groupBits}-bit RSA GoUO, ${bits}-bit Signer PK (${name})`;
 
-      it(`should verify test vector: "${str}"`, () => {
-        const ctx = new Goo(group, 2, 3, null);
-        const result = ctx.verify(msg, sig, C1);
+      it(`should verify vector: ${str}`, () => {
+        const goo = new Goo(group, g, h, null);
+        const result = goo.verify(msg, sig, C1);
+
         assert.strictEqual(result, true);
+
+        const [s_prime2, C12] = goo.decrypt(ct, key);
+
+        assert.bufferEqual(s_prime2, s_prime);
+        assert.bufferEqual(C12, C1);
+      });
+
+      it(`should not accept invalid proof: ${str}`, () => {
+        const goo = new Goo(group, g, h, null);
+        const sig2 = Buffer.from(sig);
+        const C12 = Buffer.from(C1);
+        const i = (Math.random() * sig2.length) | 0;
+        const j = (Math.random() * C12.length) | 0;
+
+        let res1, res2;
+
+        sig2[i] ^= 1;
+        res1 = goo.verify(msg, sig2, C12);
+        sig2[i] ^= 1;
+
+        assert.strictEqual(res1, false);
+
+
+        C12[j] ^= 1;
+        res2 = goo.verify(msg, sig2, C12);
+        C12[j] ^= 1;
+
+        assert.strictEqual(res2, false);
       });
     }
   });
 }
 
-runTests('Goo', Goo, Native, vectorsNative);
-runTests('Goo (Native)', Native, Goo, vectors);
+runTests('Goo', JS, Native);
+runTests('Goo (Native)', Native, JS);
