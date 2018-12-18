b232333a85c4b3674f5b3fd3008fe2cbf036c0ba
Christopher Jeffrey <chjjeffrey@gmail.com>
2018-12-18 09:24:21 -0800

    test: reorganize.

--- test/native/index.js
+++ test/native/index.js
@@ -3,7 +3,7 @@
 if (process.env.NODE_BACKEND && process.env.NODE_BACKEND !== 'native')
   return;
 
-const {Goo} = require('../lib/native/binding');
+const {Goo} = require('../../lib/native/binding');
 
 if (Goo.test)
   Goo.test();
