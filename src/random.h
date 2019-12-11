52436dc279f749f0e3d370088cda7abc5a75a84f
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-12-11 02:05:40 -0800

    src: add rng.

---
+++ src/random.h
@@ -0,0 +1,18 @@
+/*!
+ * random.cc - random number generation
+ * Copyright (c) 2018, Christopher Jeffrey (MIT License).
+ * https://github.com/handshake-org/goosig
+ */
+
+#ifndef _GOO_RANDOM_H
+#define _GOO_RANDOM_H
+
+#include <stdlib.h>
+
+void
+goo_poll(void);
+
+int
+goo_random(void *dst, size_t len);
+
+#endif
